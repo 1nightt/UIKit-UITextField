e053ace65647c4fedfc4981dedadf239fa0fdfc2
1nightt <vorypaev.04@yandex.ru>
2024-07-03 17:31:00 +0300

    Сделал ДЗ по UITextField

--- UIKit-UITextField/SceneDelegate.swift
+++ UIKit-UITextField/SceneDelegate.swift
@@ -1,11 +1,11 @@
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
     var window: UIWindow?
-
-
-    final func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         
